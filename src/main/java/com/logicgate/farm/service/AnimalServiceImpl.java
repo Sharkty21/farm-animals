dd15c387dc159da5f2d72ed140cc2441bb7fbc42
Sharkty21 <tylermsharkey@gmail.com>
2020-06-11 01:23:45 -0500

    first commit

---
+++ src/main/java/com/logicgate/farm/service/AnimalServiceImpl.java
@@ -0,0 +1,57 @@
+package com.logicgate.farm.service;
+
+import com.logicgate.farm.domain.Animal;
+import com.logicgate.farm.repository.AnimalRepository;
+import com.logicgate.farm.repository.BarnRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AnimalServiceImpl implements AnimalService {
+
+  private final AnimalRepository animalRepository;
+
+  private final BarnRepository barnRepository;
+
+  @Autowired
+  public AnimalServiceImpl(AnimalRepository animalRepository, BarnRepository barnRepository) {
+    this.animalRepository = animalRepository;
+    this.barnRepository = barnRepository;
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public List<Animal> findAll() {
+    return animalRepository.findAll();
+  }
+
+  @Override
+  public void deleteAll() {
+    animalRepository.deleteAll();
+  }
+
+  @Override
+  public Animal addToFarm(Animal animal) {
+    return animal;
+  }
+
+  @Override
+  public void addToFarm(List<Animal> animals) {
+    animals.forEach(this::addToFarm);
+  }
+
+  @Override
+  public void removeFromFarm(Animal animal) {
+    // TODO: implementation of this method
+  }
+
+  @Override
+  public void removeFromFarm(List<Animal> animals) {
+    animals.forEach(animal -> removeFromFarm(animalRepository.getOne(animal.getId())));
+  }
+}
